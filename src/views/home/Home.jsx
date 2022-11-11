8c325839ca4dd94e8f806260ce591a7220ebf360
Jacob Lapkin <65730020+Jacob-Lapkin@users.noreply.github.com>
2022-11-11 12:27:03 -0800

    added navbar

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import './Home.css'
 
 function Home() {
   return (
     <>
-      <h1>Home Page</h1>
+    <Navbar/>
+    <section className="main-container">
+    <h1>Home Page</h1>
+    </section>
     </>
   );
 }
