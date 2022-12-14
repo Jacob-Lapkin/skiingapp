0fb7e0c15fae600d51967aadc4fea87121d76614
Jacob Lapkin <65730020+Jacob-Lapkin@users.noreply.github.com>
2022-12-14 09:44:16 -0800

    added title

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,8 +7,7 @@ function Header() {
       <div className="header-container shadow p-3 d-flex flex-row justify-content-start gap-5 align-items-center">
         <div className="d-flex flex-row gap-4">
           <i className="fa-solid fa-mountain-sun fa-3x text-white"></i>
-          <i className="fa-solid fa-snowflake fa-3x text-white"></i>
-          <i className="fa-solid fa-snowman fa-3x text-white"></i>
+          <p className="h1 text-white fw-bold">SKI NOW</p>
         </div>
         <div>
           <input className="searchbar" placeholder="Search User" />
